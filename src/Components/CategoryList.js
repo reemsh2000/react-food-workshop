cf4da41878ec00774cb2a195608e53042e7175c4
reemsh2000 <sh951652@gmail.com>
2021-09-14 23:25:09 +0300

    another sol

---
+++ src/Components/CategoryList.js
@@ -0,0 +1,39 @@
+import React from "react";
+const categories = [
+  "all",
+  "burger",
+  "hot dog",
+  "sandwich",
+  "fries",
+  "topping",
+  "drink",
+  "extra",
+];
+function CategoryList(props) {
+  const {getCategory,chosencategory}=props
+  return (
+    <div className="bowl_category">
+      <fieldset>
+        <legend>Category</legend>
+        <div className="sellect_option">
+          {categories.map((category) => {
+            return (
+              <div key={category}>
+                <label htmlFor={category}>{category}</label>
+                <input
+                  type="radio"
+                  name="category"
+                  onClick={getCategory}
+                  defaultChecked={category===chosencategory}
+                  value={category}
+                  id={category}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </fieldset>
+    </div>
+  );
+}
+export default CategoryList;
